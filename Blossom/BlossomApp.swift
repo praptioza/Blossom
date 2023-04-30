ed56843f0228c0afc5f9adb37a89ba247ecb63b8
praptioza <praptioza001@gmail.com>
2023-04-30 01:20:45 -0400

    Initial Commit

---
+++ Blossom/BlossomApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlossomApp.swift
+//  Blossom
+//
+//  Created by user232715 on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct BlossomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
